738f251a3ae224379753b16b39459364717ad68e
DennisAMayo <cha0sriderx@gmail.com>
2021-09-03 04:33:29 -0400

    0.2 update

---
+++ README.md
@@ -0,0 +1,21 @@
+# PacMen
+
+This is an interactive code that allows you to add pacmen and watch them float across the screen.
+
+## Future Releases
+
+- Limit the number of setTimeouts so the pacmen dont go crazy after spamming "Start Game."
+
+## Version History
+
+* 0.2
+    * Added mouth movement.
+    * Added direction change.
+    * Added some css.
+
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the MIT License License - see the LICENSE.md file for details
--- assets/js/pacmen.js
+++ assets/js/pacmen.js
@@ -1,12 +1,10 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['./assets/images/PacMan1.png', './assets/images/PacMan2.png'],
+  ['./assets/images/PacMan3.png', './assets/images/PacMan4.png'],
 ];
-let direction = 0;
-const pacMen = []; // This array holds all the pacmen
+const pacMen = [];
 
-// This function returns an object with random values
 function setToRandom(scale) {
   return {
     x: Math.random() * scale,
@@ -14,40 +12,43 @@ function setToRandom(scale) {
   };
 }
 
-// Factory to make a PacMan at a random position with random velocity
 function makePac() {
-  // returns an object with random values scaled {x: 33, y: 21}
   let velocity = setToRandom(10); // {x:?, y:?}
   let position = setToRandom(200);
+  let mouth = 0;
+  let counter = 0;
+  let direction = 0;
 
-  // Add image to div id = game
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
-  newimg.width = 100;
+  newimg.src = './assets/images/PacMan1.png';
+  newimg.width = Math.floor(Math.random() * (150 - 50 + 1) + 50)
 
-  // TODO: set position here
   newimg.style.left = position.x;
   newimg.style.top = position.y;
-  // TODO add new Child image to game
   game.appendChild(newimg);
 
-  // return details in an object
   return {
     position,
     velocity,
     newimg,
+    counter,
+    direction,
+    mouth,
   };
 }
 
 function update() {
-  // loop over pacmen array and move each one and move image in DOM
   pacMen.forEach((item) => {
     checkCollisions(item);
     item.position.x += item.velocity.x;
     item.position.y += item.velocity.y;
-
+    item.counter++;
+    if (item.counter % 10 == 0) {
+      item.mouth = (((item.counter / 10) % 2) ? 0 : 1);
+      item.newimg.src = pacArray[item.direction][item.mouth];
+    }
     item.newimg.style.left = item.position.x;
     item.newimg.style.top = item.position.y;
   });
@@ -58,6 +59,8 @@ function checkCollisions(item) {
   if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
     item.position.x + item.velocity.x < 0) {
     item.velocity.x = -item.velocity.x;
+    item.direction = (Math.sign(item.velocity.x) == 1 ? 0 : 1 );
+    item.newimg.src = pacArray[item.direction][item.mouth]
   } 
   if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight ||
     item.position.y + item.velocity.y < 0) {
@@ -66,10 +69,5 @@ function checkCollisions(item) {
 }
 
 function makeOne() {
-  pacMen.push(makePac()); // add a new PacMan
-}
-
-//don't change this line
-if (typeof module !== 'undefined') {
-  module.exports = { checkCollisions, update, pacMen };
+  pacMen.push(makePac());
 }
